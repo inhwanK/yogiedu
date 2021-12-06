f8aca023e107fa1a3defadfdc98e15fda2a8eaba
inhwanK <inhan1009@naver.com>
2021-12-06 19:56:50 +0900

    first commit

---
+++ src/main/java/org/hustar/yogiedu/YogieduApplication.java
@@ -0,0 +1,13 @@
+package org.hustar.yogiedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogieduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogieduApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/hustar/yogiedu/controller/TestController.java
@@ -0,0 +1,14 @@
+package org.hustar.yogiedu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
