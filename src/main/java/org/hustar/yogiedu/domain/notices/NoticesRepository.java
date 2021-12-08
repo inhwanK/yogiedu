19c02d3be265e8d8cf57bdf1fb53f05b455d6dcf
inhwanK <inhan1009@naver.com>
2021-12-08 21:44:53 +0900

    jpa 의존성 추가, domain 패키지, Notices 클래스, NoticesRepository 인터페이스 추가

---
+++ src/main/java/org/hustar/yogiedu/domain/notices/NoticesRepository.java
@@ -0,0 +1,7 @@
+package org.hustar.yogiedu.domain.notices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticesRepository extends JpaRepository<Notices, Long> {
+
+}
