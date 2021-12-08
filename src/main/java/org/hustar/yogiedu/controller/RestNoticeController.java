d04d62155de66ca00c5270855cc6e41c79b72dce
inhwanK <inhan1009@naver.com>
2021-12-08 22:05:32 +0900

    jpa test 중. 500 에러 발생. SQLGrammarException 발생

--- src/main/java/org/hustar/yogiedu/controller/RestNoticeController.java
+++ src/main/java/org/hustar/yogiedu/controller/RestNoticeController.java
@@ -1,6 +1,8 @@
 package org.hustar.yogiedu.controller;
 
 import org.hustar.yogiedu.dao.NoticeDao;
+import org.hustar.yogiedu.domain.notices.Notices;
+import org.hustar.yogiedu.domain.notices.NoticesRepository;
 import org.hustar.yogiedu.dto.NoticeDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +16,13 @@ public class RestNoticeController {
 	@Autowired
 	private NoticeDao dao;
 	
+	@Autowired
+	private NoticesRepository noticeRepository;
+	
 	@GetMapping("/index")
 	public String index() {
-		NoticeDto dto = dao.getNotice(); 
-		return "api test > " + dto;
+		NoticeDto dto = dao.getNotice();
+		
+		return "api test > " + dto+"<br> jpa test"+noticeRepository.findAll();
 	}
 }
