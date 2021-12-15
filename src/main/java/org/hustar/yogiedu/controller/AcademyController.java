ac10091a787b9f4987b12c51ac4ce0363cd7ee14
kiminpyo <rladlsvy2448@naver.com>
2021-12-15 18:18:21 +0900

    academyEnroll작성

--- src/main/java/org/hustar/yogiedu/controller/AcademyController.java
+++ src/main/java/org/hustar/yogiedu/controller/AcademyController.java
@@ -2,6 +2,7 @@ package org.hustar.yogiedu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AcademyController {
@@ -9,5 +10,9 @@ public class AcademyController {
 	@GetMapping("/academySearch")
 	public String academySearch() {
 		return "academy/academySearch";
+	}
+	@GetMapping("/academyEnroll")
+	public String academyEnroll() {
+		return "academy/academyEnroll";
 	}
 }
